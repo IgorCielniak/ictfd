d4dd8bbc218655386bd653a3dbbc1d8d581006f5
IgorCielniak <igorcielniak.contact@gmail.com>
2025-01-05 22:00:37 +0100

    modified:   ictfd.py

--- ictfd.py
+++ ictfd.py
@@ -25,7 +25,6 @@ def download_http_file(url, download_dir, chunk_size=DEFAULT_CHUNK_SIZE, timeout
 
     for attempt in range(retries):
         try:
-            # Check if the file exists and it's the first attempt
             if os.path.exists(file_name) and not failed_attempt:
                 user_input = input(f"The file '{file_name}' already exists. Do you want to overwrite it? (y/n): ").lower()
                 if user_input != 'y':
@@ -66,7 +65,7 @@ def download_http_file(url, download_dir, chunk_size=DEFAULT_CHUNK_SIZE, timeout
             break
         except requests.exceptions.RequestException as e:
             print(f"Error downloading file (attempt {attempt + 1}): {e}")
-            failed_attempt = True  # Mark that a failure occurred
+            failed_attempt = True
             if attempt < retries - 1:
                 print(f"Retrying download in 5 seconds...")
                 time.sleep(5)
@@ -117,8 +116,7 @@ def display_version():
                 print(f"\nA newer version ({latest_version}) is available. You can download it from: {GITHUB_RELEASE_URL}")
                 user_input = input("Do you want to download the newer version? (y/n): ").lower()
                 if user_input == 'y':
-                    print("Downloading the newer version...")
-                    download_file(GITHUB_RELEASE_URL, custom_download_dir = os.path.dirname(os.path.abspath(__file__)))
+                    download_file(GITHUB_RELEASE_URL, download_dir = os.path.dirname(os.path.abspath(__file__)))
         except Exception as e:
             print("Failed to check for updates:", e)
 
@@ -145,91 +143,39 @@ def download_file(url, custom_chunk_size=None, custom_download_dir=None, timeout
     except Exception as e:
             print(f"Error downloading file: {e}")
 
-def interactive_mode():
-    print("\nICTFD - Interactive Mode\n")
-    print("Welcome to ICTFD (Interactive Command-Line File Downloader)!\n")
-    print("To download files, please follow these steps:")
-    print("1. Enter the number of files you want to download.")
-    print("2. For each file, enter the URL of the file.")
-    print("3. Optionally, specify a custom chunk size for downloading.")
-    print("4. Optionally, specify a custom download directory.")
-    print("5. Optionally, specify a custom timeout duration.")
-    print("6. Optionally, specify the number of retry attempts.")
-    print("7. Press Enter after each URL to proceed to the next file.\n")
-
-    try:
-        num_files = int(input("Enter the number of files to download: "))
-    except ValueError:
-        print("Invalid input. Please enter a valid number.")
-        return
-
-    for i in range(1, num_files + 1):
-        print(f"\nFile {i}:")
-        url = input("Enter the URL of the file: ")
-
-        custom_chunk_size = None
-        chunk_size_input = input("Enter custom chunk size (press Enter for default): ")
-        if chunk_size_input:
-            try:
-                custom_chunk_size = int(chunk_size_input)
-            except ValueError:
-                print("Invalid chunk size. Using default.")
-
-        custom_download_dir = input("Enter custom download directory (press Enter to use default): ")
-        custom_timeout = input("Enter custom timeout duration in seconds (press Enter for default): ")
-        if custom_timeout:
-            try:
-                custom_timeout = int(custom_timeout)
-            except ValueError:
-                print("Invalid timeout duration. Using default.")
-
-        custom_retries = input("Enter custom retry attempts (press Enter for default): ")
-        if custom_retries:
-            try:
-                custom_retries = int(custom_retries)
-            except ValueError:
-                print("Invalid retry count. Using default.")
-
-        download_file(url, custom_chunk_size, custom_download_dir, custom_timeout, custom_retries)
-
-    input("\nPress Enter to exit.")
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        interactive_mode()
+    custom_download_dir = None
+    custom_chunk_size = None
+    custom_timeout = None
+    custom_retries = None
+    url_index = 1
+
+    for index, arg in enumerate(sys.argv[1:], start=1):
+        if arg in ["-d", "--download-dir"]:
+            try:
+                custom_download_dir = sys.argv[index + 1]
+            except IndexError:
+                print("Invalid custom download directory. Using default.")
+        elif arg in ["-c", "--chunk-size"]:
+            try:
+                custom_chunk_size = int(sys.argv[index + 1])
+            except IndexError:
+                print("Invalid custom chunk size. Using default.")
+        elif arg in ["-t", "--timeout"]:
+            try:
+                custom_timeout = int(sys.argv[index + 1])
+            except IndexError:
+                print("Invalid custom timeout. Using default.")
+        elif arg in ["-r", "--retries"]:
+            try:
+                custom_retries = int(sys.argv[index + 1])
+            except IndexError:
+                print("Invalid retry count. Using default.")
+
+    if sys.argv[1] in ["-h", "--help"]:
+        display_help()
+    elif sys.argv[1] in ["-v", "--version"]:
+        display_version()
     else:
-        custom_download_dir = None
-        custom_chunk_size = None
-        custom_timeout = None
-        custom_retries = None
-        url_index = 1
-
-        for index, arg in enumerate(sys.argv[1:], start=1):
-            if arg in ["-d", "--download-dir"]:
-                try:
-                    custom_download_dir = sys.argv[index + 1]
-                except IndexError:
-                    print("Invalid custom download directory. Using default.")
-            elif arg in ["-c", "--chunk-size"]:
-                try:
-                    custom_chunk_size = int(sys.argv[index + 1])
-                except IndexError:
-                    print("Invalid custom chunk size. Using default.")
-            elif arg in ["-t", "--timeout"]:
-                try:
-                    custom_timeout = int(sys.argv[index + 1])
-                except IndexError:
-                    print("Invalid custom timeout. Using default.")
-            elif arg in ["-r", "--retries"]:
-                try:
-                    custom_retries = int(sys.argv[index + 1])
-                except IndexError:
-                    print("Invalid retry count. Using default.")
-
-        if sys.argv[1] in ["-h", "--help"]:
-            display_help()
-        elif sys.argv[1] in ["-v", "--version"]:
-            display_version()
-        else:
-            download_file(sys.argv[url_index], custom_chunk_size, custom_download_dir, custom_timeout, custom_retries)
-
+        download_file(sys.argv[url_index], custom_chunk_size, custom_download_dir, custom_timeout, custom_retries)
